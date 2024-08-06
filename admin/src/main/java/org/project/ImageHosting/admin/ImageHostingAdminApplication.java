fbdd51e1b045d5b50b448c412873c25245e47e93
Geng Zhigao <zhigaogeng.johnson@gmail.com>
2024-08-06 13:40:37 +0800

    Launch Admin

---
+++ admin/src/main/java/org/project/ImageHosting/admin/ImageHostingAdminApplication.java
@@ -0,0 +1,11 @@
+package org.project.ImageHosting.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageHostingAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ImageHostingAdminApplication.class, args);
+    }
+}
